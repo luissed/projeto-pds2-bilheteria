6913a005e845f62eac9cb71864a51bf6b907d4e4
JPCopelli <copelli.eng@gmail.com>
2023-11-30 09:10:35 -0300

    Headers funcionario.hpp e funções implementadas (alterarNome e alterarSenha)

--- include/funcionario.hpp
+++ include/funcionario.hpp
@@ -1,1 +1,39 @@
+#ifndef FUNCIONARIO_HPP
+#define FUNCIONARIO_HPP
+
+#include <string>
 #include <iostream>
+#include <iomanip>
+
+
+enum Cargos
+{
+    CAIXA,
+    ORGANIZADOR,
+    GERENTE,
+    SEM_CARGO
+};
+
+
+class Funcionario{
+    private:
+    unsigned long int _id;
+    std::string _nome;
+    std::string _senha;
+    Cargos _cargoFuncionario;
+    
+    public:
+    Funcionario(unsigned long int id, std::string nome, std::string senha, Cargos cargoFuncionario) :
+    _id(id), _nome(nome), _senha(senha), _cargoFuncionario(cargoFuncionario) {};
+    bool alterarNome(std::string novoNome);
+    bool alterarSenha(std::string novaSenha);
+    bool alterarCargo(unsigned short int novoCargo);
+    void exibirFuncionario(void);
+    bool realizarLogin(unsigned long int id, std::string senha);
+    long unsigned int getId();
+    std::string getNome();
+    std::string getSenha();
+    Cargos getCargo(void);
+};
+
+#endif
--- src/funcionario.cpp
+++ src/funcionario.cpp
@@ -1,1 +1,21 @@
-#include <iostream>
+#include "funcionario.hpp"
+
+bool Funcionario::alterarNome(std::string novoNome){
+    if(!novoNome.empty()){
+        this->_nome=novoNome;
+        std::cout<<"NOME ALTERADO COM SUCESSO"<<std::endl;
+        return true;
+    }
+    std::cout<<"NOME INVALIDO"<<std::endl;
+    return false;
+}
+
+bool Funcionario::alterarSenha(std::string novaSenha){
+    if(!novaSenha.empty()){
+        this->_senha=novaSenha;
+        std::cout<<"SENHA ALTERADA COM SUCESSO"<<std::endl;
+        return true;
+    }
+    std::cout<<"SENHA INVALIDA"<<std::endl;
+    return false;
+}
