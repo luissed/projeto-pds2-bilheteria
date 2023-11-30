28155f6a77abe0ae10b2e91ecc244b3e193c7fe9
Jose Roberto <jrfelicio9@gmail.com>
2023-11-30 19:45:24 -0300

    cliente .hpp feito

--- include/cliente.hpp
+++ include/cliente.hpp
@@ -1,1 +1,25 @@
-#include <iostream> // cliente.hpp
+#ifndef CLIENTE_HPP
+#define CLIENTE_HPP
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+class Cliente{
+    private:
+    std::string _nome;
+    std::string _email;
+    unsigned short int _idade;
+    std::string _cpf;
+    public:
+    Cliente(std::string nome, std::string email, unsigned short int idade, std::string cpf) :
+    _nome(nome), _email(email), _idade(idade), _cpf(cpf) {};
+    std::string getNome();
+    std::string getEmail();
+    unsigned short int getIdade();
+    std::string getCpf();
+    std::stringstream exibirCliente(void);
+};
+
+#endif
