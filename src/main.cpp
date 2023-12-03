1188040873352162187726a34e421df5214b8f08
Luis Eduardo <luis.eduardod2012@gmail.com>
2023-12-03 11:10:47 -0300

    Acabei a main
    Estou aguardando o José Roberto terminar compras

--- src/main.cpp
+++ src/main.cpp
@@ -118,6 +118,162 @@ int main() {
                 default:
                     std::cout << "OPCAO INVALIDA" << std::endl;
                 }
-            }
+            }while(opcao!='l'&&opcao!='L');
+            break;
+            case ORGANIZADOR:
+            do{
+                std::cin.ignore();
+                std::cout << "Pressione Enter para continuar...";
+                std::cin.get();
+                system("clear");
+                std::cout<<"MENU ORGANIZADOR"<<std::endl;
+                std::cout<<"a. cadastro filme\tb. editar filme\tc. retirar filme do cartaz"<<std::endl;
+                std::cout<<"d. mostrar filmes em cartaz\te. mostrar todos filmes \tf. criar sessao"<<std::endl;
+                std::cout<<"g. trocar sala\th. mostrar sessoes\ti. mostrar sessoes ativas"<<std::endl;
+                std::cout<<"j. mostrar salas ativas\tl. logout"<<std::endl;
+                std::cin>>opcao;
+                switch (toupper(opcao)) {
+                case 'A':
+                    std::cout<<"INSIRA OS DADOS DO FILME"<<std::endl;
+                    std::cout<<"TITULO: ";
+                    getline(std::cin,titulo);
+                    std::cout<<"DURACAO EM MINUTOS: ";
+                    std::cin>>duracaoM;
+                    std::cout<<"GENERO: ";
+                    std::cin>>genero;
+                    std::cout<<"CLASSIFICACAO: ";
+                    std::cin>>classificacao;
+                    switch (toupper(classificacao[1])) {
+                    case 'I':
+                        servico.adicionarFilme(duracaoM, titulo, LIVRE, genero);
+                    break;
+                    case '2':
+                        servico.adicionarFilme(duracaoM, titulo, _12, genero);
+                    break;
+                    case '4':
+                        servico.adicionarFilme(duracaoM, titulo, _14, genero);
+                    break;
+                    case '6':
+                        servico.adicionarFilme(duracaoM, titulo, _16, genero);
+                    break;
+                    case '8':
+                        servico.adicionarFilme(duracaoM, titulo, _18, genero);
+                    break;
+                    default:
+                        std::cout << "CLASSIFICACAO INVALIDA." << std::endl;
+                        servico.adicionarFilme(duracaoM, titulo, LIVRE, genero);
+                    break;
+                    }
+                break;
+                case 'B':
+                    std::cout<<"INSIRA O ID DO FILME"<<std::endl;
+                    std::cout<<"ID: ";
+                    std::cin>>idFilme;
+                    servico.editarFilme(idFilme);
+                break;
+                case 'C':
+                    std::cout<<"INSIRA O ID DO FILME"<<std::endl;
+                    std::cout<<"ID: ";
+                    std::cin>>idFilme;
+                    servico.removerFilmeCartaz(idFilme);
+                break;
+                case 'D':
+                    servico.exibirFilmesEmCartaz();
+                break;
+                case 'E':
+                    servico.exibirFilmes();
+                break;
+                case 'F':
+                    std::cout<<"INSIRA OS ID'S DA SALA E DO FILME "<<std::endl;
+                    std::cout<<"ID SALA: ";
+                    std::cin>>idSala;
+                    std::cout<<"ID FILME: ";
+                    std::cin>>idFilme;
+                    std::cout<<"INSIRA O HORARIO "<<std::endl;
+                    std::cout<<"ANO(YYYY): ";
+                    std::cin>>horario.tm_year;
+                    std::cout<<"MES(M): ";
+                    std::cin>>horario.tm_mon;
+                    std::cout<<"DIA(D): ";
+                    std::cin>>horario.tm_mday;
+                    std::cout<<"HORA(H): ";
+                    std::cin>>horario.tm_hour;
+                    std::cout<<"MINUTOS(M): ";
+                    std::cin>>horario.tm_min;
+                    servico.criarSessao(idSala, idFilme, horario);
+                break;
+                case 'G':
+                    std::cout<<"INSIRA OS ID'S DA SALA E DA SESSAO "<<std::endl;
+                    std::cout<<"ID SALA: ";
+                    std::cin>>idSala;
+                    std::cout<<"ID SESSAO: ";
+                    std::cin>>idSessao;
+                    servico.trocarSalaSessao(idSala, idSessao);
+                break;
+                case 'H':
+                    servico.exibirSessoes();
+                break;
+                case 'I':
+                    servico.exibirSessoesAtivas();
+                break;
+                case 'J':
+                    servico.mostrarSalasAtivas();
+                break;
+                case 'L':
+                    std::cout << "LOGOUT" << std::endl;
+                break;
+                default:
+                    std::cout << "OPCAO INVALIDA" << std::endl;
+                }    
+            }while(opcao!='l'&&opcao!='L');
+            break;
+            case CAIXA:
+            do{
+                std::cin.ignore();
+                std::cout << "Pressione Enter para continuar...";
+                std::cin.get();
+                system("clear");
+                std::cout<<"MENU CAIXA"<<std::endl;
+                std::cout<<"a. cadastro cliente\tb. exibir sessoes\tc. realizar venda"<<std::endl;
+                std::cout<<"l. logout"<<std::endl;
+                std::cin>>opcao;
+                switch (toupper(opcao)) {
+                case 'A':
+                    std::cout<<"INSIRA OS DADOS DO CLIENTE"<<std::endl;
+                    std::cout<<"NOME: ";
+                    getline(std::cin,nome);
+                    std::cout<<"EMAIL: ";
+                    std::cin>>email;
+                    std::cout<<"IDADE: ";
+                    std::cin>>idade;
+                    std::cout<<"CPF: ";
+                    validarCPF(&cpf);
+                    servico.adicionarCliente(nome, email, idade, cpf);
+                break;
+                case 'B':
+                    servico.exibirSessoesFilmes();
+                break;
+                case 'C':
+                    std::cout<<"INSIRA OS DADOS DO CLIENTE"<<std::endl;
+                    std::cout<<"NOME: ";
+                    getline(std::cin,nome);
+                    std::cout<<"CPF: ";
+                    validarCPF(&cpf);
+                    std::cout<<"INSIRA O ID DA SESSAO"<<std::endl;
+                    std::cout<<"ID SESSAO: ";
+                    std::cin>>idSessao;
+                    servico.venderIngressos(id, idSessao, nome, cpf);
+                break;
+                case 'L':
+                    std::cout << "LOGOUT" << std::endl;
+                break;
+                default:
+                    std::cout << "OPCAO INVALIDA" << std::endl;
+                }  
+            }while(opcao!='l'&&opcao!='L');
+            break;
+        }
+        
+    }
     return 0;
 }
